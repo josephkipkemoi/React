71b833e7ed0d64c2c49d415e6d2b9bae6d302fd3
josephkipkemoi <jkemboe@gmail.com>
2022-05-09 09:53:38 -0700

    sentry

--- src/index.jsx
+++ src/index.jsx
@@ -9,14 +9,14 @@ import { Container, ThemeProvider } from 'react-bootstrap';
 import { store } from './Services/Store/Store';
 import { Provider } from 'react-redux';
 // Error monitoring and reporting
-if(process.env.NODE_ENV !== 'production') {
-  Sentry.init({
-    // dsn: process.env.production.local.REACT_APP_SENTRY_URL,
-    dsn :'https://o1227796.ingest.sentry.io/api/6373454/security/?sentry_key=4150ddd424ed4502a6afc5ae937851aa',
-    integrations: [new BrowserTracing()],
-    tracesSampleRate: 0.2
-  })
-}
+// if(process.env.NODE_ENV !== 'production') {
+//   Sentry.init({
+//     // dsn: process.env.production.local.REACT_APP_SENTRY_URL,
+//     dsn :'https://o1227796.ingest.sentry.io/api/6373454/security/?sentry_key=4150ddd424ed4502a6afc5ae937851aa',
+//     integrations: [new BrowserTracing()],
+//     tracesSampleRate: 0.2
+//   })
+// }
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') 
